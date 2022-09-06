2113a1da791f7c5d29103b91b8f204ca3744d6b1
Vivian Lu <97366023+lu279463768@users.noreply.github.com>
2022-09-06 10:37:01 -0400

    Add files via upload

--- HailstoneRevisited/src/Hailstone3.java
+++ HailstoneRevisited/src/Hailstone3.java
@@ -1,11 +1,11 @@
-import components.naturalnumber.NaturalNumber;
-import components.naturalnumber.NaturalNumber2;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 
 /**
+ * ask the user for a positive integer and then compute and output the
+ * corresponding Hailstone series.
  *
  * @author Vivian Lu
  *
@@ -20,59 +20,38 @@ public final class Hailstone3 {
 
     /**
      * Generates and outputs the Hailstone series starting with the given
-     * {@code NaturalNumber}.
+     * integer and record the length of this series and output the maxium value
+     * of the series
      *
      * @param n
-     *            the starting natural number
+     *            the starting integer
      * @param out
      *            the output stream
-     * @updates out.content
-     * @requires n > 0 and out.is_open
-     * @ensures out.content = #out.content * [the Hailstone series starting with
-     *          n]
      */
-    private static void generateSeries(NaturalNumber n, SimpleWriter out) {
-        NaturalNumber one = new NaturalNumber2(1);
-        NaturalNumber two = new NaturalNumber2(2);
-        NaturalNumber three = new NaturalNumber2(3);
-        NaturalNumber temp = new NaturalNumber2(n);
-        NaturalNumber max = new NaturalNumber2();
-
-        out.println(temp);
-        int count = 1;
-
-        while (temp.compareTo(one) != 0) {
-            if (temp.compareTo(max) > 0) {
-                max.copyFrom(temp);
+    private static void generateSeries(int n, SimpleWriter out) {
+        int len = 1;
+        int max = n;
+        while (n != 1) {
+            out.print(n + " ");
+            len++;
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else if (n % 2 == 1) {
+                n = 3 * n + 1;
             }
-            NaturalNumber r = temp.divide(two);//可以直接用dividedBy10()直接就可的得到int型的个位数 然后multiplyBy10(remainder)
-
-            if (r.isZero()) {//then it's even
-                out.println(temp);
-                count++;
-            } else if (!r.isZero()) {
-                /**
-                 * if it's not even we first should set temp back to its
-                 * original
-                 */
-                temp.multiply(two);
-                temp.add(r);
-
-                temp.multiply(three);
-                temp.add(one);
-                out.println(temp);
-                count++;
-
+            if (n > max) {
+                max = n;
             }
-
         }
-        out.println("The length of the series is " + count);
-        out.println("The max value in the series is " + max);
+        out.print(n + " ");
+        out.println();
+        out.println("The length of series is " + len);
+        out.println("The max value is " + max);
+
     }
 
     /**
-     * Main method. print the value of the NaturalNumber after the call to
-     * generateSeries to confirm that the value was restored.
+     * Main method.
      *
      * @param args
      *            the command line arguments
@@ -80,12 +59,13 @@ public final class Hailstone3 {
     public static void main(String[] args) {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
-
-        out.print("Please give a positive integer: ");
+        /*
+         * Put your main program code here, ask the user for a positive integer
+         */
+        out.print("Please input a positive integer number:");
         int input = in.nextInteger();
-        NaturalNumber n = new NaturalNumber2(input);
+        generateSeries(input, out);
 
-        generateSeries(n, out);
         /*
          * Close input and output streams
          */
--- HailstoneRevisited/src/Hailstone4.java
+++ HailstoneRevisited/src/Hailstone4.java
@@ -10,17 +10,18 @@ import components.simplewriter.SimpleWriter1L;
  * @author Vivian Lu
  *
  */
-public final class Hailstone2 {
+public final class Hailstone4 {
 
     /**
      * Private constructor so this utility class cannot be instantiated.
      */
-    private Hailstone2() {
+    private Hailstone4() {
     }
 
     /**
      * Generates and outputs the Hailstone series starting with the given
-     * integer and record the length of this series
+     * integer and record the length of this series and output the maxium value
+     * of the series
      *
      * @param n
      *            the starting integer
@@ -29,7 +30,9 @@ public final class Hailstone2 {
      */
     private static void generateSeries(int n, SimpleWriter out) {
         int len = 1;
+        int max = n;
         while (n != 1) {
+
             out.print(n + " ");
             len++;
             if (n % 2 == 0) {
@@ -37,10 +40,14 @@ public final class Hailstone2 {
             } else if (n % 2 == 1) {
                 n = 3 * n + 1;
             }
+            if (n > max) {
+                max = n;
+            }
         }
         out.print(n + " ");
         out.println();
         out.println("The length of series is " + len);
+        out.println("The max value is " + max);
 
     }
 
@@ -53,13 +60,24 @@ public final class Hailstone2 {
     public static void main(String[] args) {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
-        /*
-         * Put your main program code here, ask the user for a positive integer
-         */
-        out.print("Please input a positive integer number:");
-        int input = in.nextInteger();
-        generateSeries(input, out);
 
+        /*
+         * Change it so that it repeatedly asks the user whether they wish to
+         * calculate another series. If the response is "y", then the program
+         * should proceed; if it is anything else, then the program should quit.
+         */
+        out.print("Do you want to calculate another series?");
+        String userChoice = in.nextLine();
+        while (userChoice.charAt(0) == 'y') {
+            /*
+             * ask the user for a positive integer
+             */
+            out.print("Please input a positive integer number:");
+            int input = in.nextInteger();
+            generateSeries(input, out);
+            out.print("Do you want to calculate another series?");
+            userChoice = in.nextLine();
+        }
         /*
          * Close input and output streams
          */
